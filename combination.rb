783e5fd1e44b7f64d2795344d2eafd912486dcf9
Ajit <gajit@hiro.ajitgeorge.com>
2008-12-18 22:11:35 -0600

    all tests pass, somehow

--- combination.rb
+++ combination.rb
@@ -1,2 +1,26 @@
 def combine(sets)
+  return [] if sets.empty? 
+
+  if sets.size == 1 then
+    combinations = []
+    sets[0].each do |elem| 
+      combinations << [elem] 
+    end
+    return combinations
+  end
+
+  combinations = []
+
+  
+  if !sets.empty? then
+    set = sets[0]
+    rest = sets[1..-1]
+    set.each do |elem|
+      combine(rest).each do |c|
+        combinations << [elem, c].flatten
+      end
+    end
+  end
+  
+  combinations
 end
